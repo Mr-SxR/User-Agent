6d9a1cf166b3c29fb77fa5ed6a95a0427e2a6630
Masudur Rahman Sifat <168710908+Mr-SxR@users.noreply.github.com>
2025-07-08 21:10:27 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const repoName = "User-Agent";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? `/${repoName}/` : '/',
   server: {
     host: "::",
     port: 8080,
